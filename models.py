3bc7740b41ad4ddcfe55b7aabdc94ce5f3738810
DanielleM27 <65420682+DanielleM27@users.noreply.github.com>
2021-12-01 01:09:31 +0200

    Update models.py

--- models.py
+++ models.py
@@ -9,10 +9,10 @@ db = SQLAlchemy(app)
 
 class Project(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column("Project Title", db.String())
-    date = db.Column("Project Date", db.DateTime)
-    description = db.Column("Project Description", db.Text)
-    skills = db.Column("Skills Used", db.Text)
+    title = db.Column("Title", db.String())
+    date = db.Column("Date", db.DateTime)
+    description = db.Column("Description", db.Text)
+    skills = db.Column("Skills", db.Text)
     url = db.Column('GitHub Repo Link', db.Text)
 
     def __repr__(self):
