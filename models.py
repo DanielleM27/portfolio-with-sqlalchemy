e40a204f25b44c4e64bce6c97859b6de41cddaf3
DanielleM27 <65420682+DanielleM27@users.noreply.github.com>
2021-12-05 19:46:05 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import date, datetime
+from datetime import date
 import datetime
 import time
 
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 class Project(db.Model):
     id = db.Column("Id", db.Integer, primary_key=True)
     title = db.Column("Title", db.String())
-    date = db.Column("Date", db.DateTime)
+    date = db.Column("Date", db.DateTime, default=datetime.date.fromisoformat("2020-01-31"), index=True)
     description = db.Column("Description", db.Text)
     skills = db.Column("Skills", db.Text)
     url = db.Column('GitHub Repo Link', db.Text)
